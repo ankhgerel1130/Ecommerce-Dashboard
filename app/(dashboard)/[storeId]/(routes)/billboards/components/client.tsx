6602ea1b671568a817dc83b9e3e8962f50efe110
ankhgerel1130 <enkhturankhgerel@gmail.com>
2025-03-31 18:12:11 +0800

    a

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -16,7 +16,7 @@ export const BillboardClient = () => {
         <div className="flex items-center justify-between">
             <Heading 
             title="Billboards (0)"
-            description="Manage billboards for your store"
+            description="Pizda"
             />
             <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
                 <Plus className="mr-2 h-4 w-4" />
