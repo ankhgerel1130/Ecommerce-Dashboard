8f4ae564601e74886654dae790daf521fcecb0a4
ankhgerel1130 <enkhturankhgerel@gmail.com>
2025-03-31 22:38:40 +0800

    Sizes

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -0,0 +1,37 @@
+import { BillboardClient } from "./components/client";
+import prismadb from "@/lib/prismadb";
+import { BillboardColumn } from "./components/columns";
+import {format} from "date-fns";
+interface BillboardsPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const BillboardsPage = async ({ params }: BillboardsPageProps) => {
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, "yyyy - MMMM - do")
+  }))
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardClient data={formattedBillboards} />
+      </div>
+    </div>
+  );
+};
+
+export default BillboardsPage;
